6bddb6c92889d89f76e69d3d6aa714810587d559
Dietmar <dietmar.kling@googlemail.com>
2020-07-06 16:06:34 +0200

    update

--- roles/proxmox-create-kvm/filter_plugins/collections.py
+++ roles/proxmox-create-kvm/filter_plugins/collections.py
@@ -1,15 +1,32 @@
+#import netaddr
 from netaddr import IPAddress
+import collections
 
 class FilterModule(object):
     '''
     custom jinja2 filters for working with collections
     '''
-
     def filters(self):
         return {
             'cidr_to_netmask': cidr_to_netmask,
-            'netmask_to_cidr': netmask_to_cidr
+            'netmask_to_cidr': netmask_to_cidr,
+            'duplicate':       duplicate
         }
+
+def duplicate(a):
+    if isinstance(a, collections.Hashable):
+        c = set(a)
+    else:
+        s = {}
+        c = []
+        for x in a:
+            if x not in s:
+                s[x] = 1
+            else:
+                if s[x] == 1:
+                    c.append(x)
+                s[x] += 1
+    return c
 
 def cidr_to_netmask(collection):
     len = collection
@@ -18,4 +35,4 @@ def cidr_to_netmask(collection):
     return result
 
 def netmask_to_cidr(collection):
-    return IPAddress(collection).netmask_bits()
+    return netaddr.IPAddress(collection).netmask_bits()
