340131f908d6c759a2d38fc67ad108c98bba214d
Dietmar <dietmar.kling@googlemail.com>
2020-07-18 09:13:45 +0200

    update

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd "$DIRNAME"
 CONFIG_NAME=fritz.box
 CONFIG_STAGE=prod
 CONFIG="-e config_name=$CONFIG_NAME -e config_stage=$CONFIG_STAGE"
-ansible-playbook playbook.yaml -i inventory.yaml $CONFIG $*
+ansible-playbook playbook.yaml -i inventory.yaml $CONFIG "${@}"
