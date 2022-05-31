a15507480273c626ef0ee0a0240f33d033e1b079
Dietmar Kling <dietmar.kling@googlemail.com>
2022-05-31 18:19:18 +0200

    updates

--- bin/run.sh
+++ bin/run.sh
@@ -2,40 +2,68 @@
 set -eux
 BASENAME=$(basename -s .sh "$0")
 ARRAY=(${BASENAME//-/ })
+echo "# = ${#ARRAY[@]}"
 
+if [ ${#ARRAY[@]} == 2 ];
+then
+  echo "hosts!!!"
+  CONFIG_NAME="fritz.box"
+  CONFIG_STAGE=""
+  ACTION=ansible-playbook
+  TARGET=playbook-hosts.yaml
+  LIST=($(cat<<EOF|grep -v ^#
+inventory/${CONFIG_NAME}/all-hosts-ip.yaml
+inventory/${CONFIG_NAME}/all-hosts.yaml
+inventory/${CONFIG_NAME}/all-guests.yaml
+inventory/${CONFIG_NAME}/groups-kubespray.yaml
+inventory/${CONFIG_NAME}/groups-proxmox.yaml
+inventory/${CONFIG_NAME}/groups-available.config
+inventory/${CONFIG_NAME}/proxmox-hosts.yaml
+inventory/${CONFIG_NAME}/proxmox-guests.yaml
+inventory/${CONFIG_NAME}/groups-assembler.config
+EOF
+))
+
+elif  [ ${#ARRAY[@]} -gt 2 ]; then
+  CONFIG_NAME=${ARRAY[1]}
+  CONFIG_STAGE=${ARRAY[2]}
+  ACTION=ansible-playbook
+LIST=($(cat<<EOF|grep -v ^#
+inventory/${CONFIG_NAME}/all-hosts-ip.yaml
+inventory/${CONFIG_NAME}/all-hosts.yaml
+inventory/${CONFIG_NAME}/all-guests.yaml
+inventory/${CONFIG_NAME}/groups-kubespray.yaml
+inventory/${CONFIG_NAME}/groups-proxmox.yaml
+inventory/${CONFIG_NAME}/groups-available.config
+inventory/${CONFIG_NAME}/proxmox-hosts.yaml
+inventory/${CONFIG_NAME}/proxmox-guests.yaml
+inventory/${CONFIG_NAME}/groups-assembler.config
+inventory/${CONFIG_NAME}/${CONFIG_STAGE}/keepalived.yaml
+EOF
+))
 ACTION=${ARRAY[0]}
-CONFIG_NAME=${ARRAY[1]}
-CONFIG_STAGE=${ARRAY[2]}
 
 [[ "${ARRAY[0]}" == run  ]] && ACTION=ansible-playbook  TARGET=playbook.yaml
 [[ "${ARRAY[0]}" == test ]] && ACTION=ansible-inventory TARGET=--list
 
-LIST=($(cat<<EOF
-inventory/all.yaml
-inventory/groups-kubespray.yaml
-inventory/groups-proxmox.yaml
-inventory/groups-available.config
-inventory/proxmox_hosts.yaml
-inventory/proxmox_guests.yaml
-inventory/groups-assembler.config
-EOF
-))
+
+fi
+
 
 ITEMS=()
 for FILEPATH in "${LIST[@]}"; do
-	if [ ! -e "${FILEPATH}" ]; then
-		echo "$FILEPATH not found." >&2
-		exit 1
-	else 
-		ITEMS+=(-i)
-		ITEMS+=(${FILEPATH})
-	fi
+        if [ ! -e "${FILEPATH}" ]; then
+                echo "$FILEPATH not found." >&2
+                exit 1
+        else
+                ITEMS+=(-i)
+                ITEMS+=(${FILEPATH})
+        fi
 done
 
 $ACTION \
-	"${ITEMS[@]}"\
-	-e config_name=$CONFIG_NAME \
-	-e config_stage=$CONFIG_STAGE \
-	"${TARGET}" \
-	"$@"
-
+        "${ITEMS[@]}"\
+        -e config_name=$CONFIG_NAME \
+        -e config_stage=$CONFIG_STAGE \
+        "${TARGET}" \
+        "$@"
