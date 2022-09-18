1cba02b310a7424e4a876ded395f559815de1257
Dietmar Kling <dietmar.kling@googlemail.com>
2022-09-18 09:54:27 +0000

    add upper version limit as otherwise proxmox refuses to start the lxc container

--- roles/fedora-container-sync/files/fedora-container-sync-external.sh
+++ roles/fedora-container-sync/files/fedora-container-sync-external.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 #TARGET_DIR="/var/lib/vz/template/cache"
 #TARGET_DIR="/home/dietmar/tmp"
 TARGET_DIR="/mnt/pve/shared/template/cache"
 
+# Support of fedora versions for proxmox is limited by the Fedora.pm module, take this as upper limit
+MAX_FEDORA_VERSION=$(grep "unsupported Fedora release" /usr/share/perl5/PVE/LXC/Setup/Fedora.pm |awk '{print $NF}'|grep -oP "[0-9]+")
 MAX=3
 URL0=https://kojipkgs.fedoraproject.org/packages/Fedora-Container-Base/
 
-FEDORA_VERSION=$(curl -s "${URL0}" |grep -Po '(?<=href=")[^"]*'|grep -P "^[0-9]+/$"|sort -Vr |head -1)
-URL1=${URL0}${FEDORA_VERSION}
+FEDORA_VERSION=$(curl -s "${URL0}" |grep -Po '(?<=href=")[^"]*' |grep -oP "^[0-9]+"|sort -Vr |head -1)
+if [ $FEDORA_VERSION -gt $MAX_FEDORA_VERSION ]; then
+  FEDORA_VERSION=$MAX_FEDORA_VERSION
+fi
+exit
+URL1=${URL0}${FEDORA_VERSION}/
 LATEST=$(curl -s $URL1|grep -Po '(?<=href=")2[^"]*'|sort -Vr |head -1)
 URL2="${URL1}${LATEST}images/"
 BASENAME=$(curl -s "$URL2"|grep -Po '(?<=href=")[^"]*'|grep -E "\\.x86_64\\.tar\\.xz$"|sort -Vr|head -1)
@@ -25,4 +31,3 @@ fi
 find "${TARGET_DIR}" -type f -name "*.x86_64.tar.xz"\
 | sort -Vr | awk -v MAX=${MAX} '{ if ( NR > MAX ) print }'\
 | while read -r FILENAME; do rm -f "${FILENAME}" ; done
-
